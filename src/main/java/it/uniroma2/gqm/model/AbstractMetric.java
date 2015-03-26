225e3267b726547402b704e2fb2e2801ee2372bb
matteoluzzi <matteo.luzzi@gmail.com>
2015-03-26 12:06:24 +0100

    removed abstract qualifier from abstractmetric due to a bean instantiation problem in QuestionMetricPK

--- src/main/java/it/uniroma2/gqm/model/AbstractMetric.java
+++ src/main/java/it/uniroma2/gqm/model/AbstractMetric.java
@@ -34,7 +34,7 @@ import org.appfuse.model.User;
 		  @NamedQuery(name = "findByMeasurementScale", query = "select m from AbstractMetric m where m.measurementScale.id = :measurementScaleId") })
 @DiscriminatorColumn(name = "complexMetricType")
 @Table(name = "AbstractMetric")
-public abstract class AbstractMetric extends BaseObject
+public class AbstractMetric extends BaseObject
 {
 
 	 private static final long serialVersionUID = -4627758431767217159L;
@@ -406,5 +406,140 @@ public abstract class AbstractMetric extends BaseObject
 		  System.out.println("Metric: " + this.getCode() + " 17" + " value= " + value);
 		  return value;
 	 }
+
+	 @Override
+	 public int hashCode()
+	 {
+		  final int prime = 31;
+		  int result = 1;
+		  result = prime * result + ((actualValue == null) ? 0 : actualValue.hashCode());
+		  result = prime * result + ((code == null) ? 0 : code.hashCode());
+		  result = prime * result + ((collectingType == null) ? 0 : collectingType.hashCode());
+		  result = prime * result + ((composerFor == null) ? 0 : composerFor.hashCode());
+		  result = prime * result + ((formula == null) ? 0 : formula.hashCode());
+		  result = prime * result + ((hypothesis == null) ? 0 : hypothesis.hashCode());
+		  result = prime * result + ((id == null) ? 0 : id.hashCode());
+		  result = prime * result + ((measurementScale == null) ? 0 : measurementScale.hashCode());
+		  result = prime * result + ((measurements == null) ? 0 : measurements.hashCode());
+		  result = prime * result + ((metricOwner == null) ? 0 : metricOwner.hashCode());
+		  result = prime * result + ((name == null) ? 0 : name.hashCode());
+		  result = prime * result + ((project == null) ? 0 : project.hashCode());
+		  result = prime * result + ((satisfyingConditionOperation == null) ? 0 : satisfyingConditionOperation.hashCode());
+		  result = prime * result + ((satisfyingConditionValue == null) ? 0 : satisfyingConditionValue.hashCode());
+		  result = prime * result + ((type == null) ? 0 : type.hashCode());
+		  result = prime * result + ((unit == null) ? 0 : unit.hashCode());
+		  return result;
+	 }
+
+	 @Override
+	 public boolean equals(Object obj)
+	 {
+		  if (this == obj)
+				return true;
+		  if (obj == null)
+				return false;
+		  if (getClass() != obj.getClass())
+				return false;
+		  AbstractMetric other = (AbstractMetric) obj;
+		  if (actualValue == null)
+		  {
+				if (other.actualValue != null)
+					 return false;
+		  } else if (!actualValue.equals(other.actualValue))
+				return false;
+		  if (code == null)
+		  {
+				if (other.code != null)
+					 return false;
+		  } else if (!code.equals(other.code))
+				return false;
+		  if (collectingType != other.collectingType)
+				return false;
+		  if (composerFor == null)
+		  {
+				if (other.composerFor != null)
+					 return false;
+		  } else if (!composerFor.equals(other.composerFor))
+				return false;
+		  if (formula == null)
+		  {
+				if (other.formula != null)
+					 return false;
+		  } else if (!formula.equals(other.formula))
+				return false;
+		  if (hypothesis == null)
+		  {
+				if (other.hypothesis != null)
+					 return false;
+		  } else if (!hypothesis.equals(other.hypothesis))
+				return false;
+		  if (id == null)
+		  {
+				if (other.id != null)
+					 return false;
+		  } else if (!id.equals(other.id))
+				return false;
+		  if (measurementScale == null)
+		  {
+				if (other.measurementScale != null)
+					 return false;
+		  } else if (!measurementScale.equals(other.measurementScale))
+				return false;
+		  if (measurements == null)
+		  {
+				if (other.measurements != null)
+					 return false;
+		  } else if (!measurements.equals(other.measurements))
+				return false;
+		  if (metricOwner == null)
+		  {
+				if (other.metricOwner != null)
+					 return false;
+		  } else if (!metricOwner.equals(other.metricOwner))
+				return false;
+		  if (name == null)
+		  {
+				if (other.name != null)
+					 return false;
+		  } else if (!name.equals(other.name))
+				return false;
+		  if (project == null)
+		  {
+				if (other.project != null)
+					 return false;
+		  } else if (!project.equals(other.project))
+				return false;
+		  if (questions == null)
+		  {
+				if (other.questions != null)
+					 return false;
+		  } else if (!questions.equals(other.questions))
+				return false;
+		  if (satisfyingConditionOperation != other.satisfyingConditionOperation)
+				return false;
+		  if (satisfyingConditionValue == null)
+		  {
+				if (other.satisfyingConditionValue != null)
+					 return false;
+		  } else if (!satisfyingConditionValue.equals(other.satisfyingConditionValue))
+				return false;
+		  if (type != other.type)
+				return false;
+		  if (unit == null)
+		  {
+				if (other.unit != null)
+					 return false;
+		  } else if (!unit.equals(other.unit))
+				return false;
+		  return true;
+	 }
+
+	 @Override
+	 public String toString()
+	 {
+		  return "AbstractMetric [id=" + id + ", code=" + code + ", name=" + name + "]";
+	 }
+
+	 
 
 }
--- src/main/java/it/uniroma2/gqm/model/CombinedMetric.java
+++ src/main/java/it/uniroma2/gqm/model/CombinedMetric.java
@@ -53,34 +53,30 @@ public class CombinedMetric extends AbstractMetric
 	 public int hashCode()
 	 {
 		  final int prime = 31;
-		  int result = 1;
+		  int result = super.hashCode();
 		  result = prime * result + ((composedBy == null) ? 0 : composedBy.hashCode());
-		  result = prime * result + ((actualValue == null) ? 0 : actualValue.hashCode());
-		  result = prime * result + ((code == null) ? 0 : code.hashCode());
-		  result = prime * result + ((collectingType == null) ? 0 : collectingType.hashCode());
-		  result = prime * result + ((hypothesis == null) ? 0 : hypothesis.hashCode());
-		  result = prime * result + ((measurementScale == null) ? 0 : measurementScale.hashCode());
-		  result = prime * result + ((measurements == null) ? 0 : measurements.hashCode());
-
-		  result = prime * result + ((metricOwner == null) ? 0 : metricOwner.hashCode());
-		  result = prime * result + ((name == null) ? 0 : name.hashCode());
-
-		  result = prime * result + ((project == null) ? 0 : project.hashCode());
-		  /*
-		   * result = prime * result + ((questions == null) ? 0 :
-		   * questions.hashCode());
-		   */
-		  result = prime * result + ((satisfyingConditionOperation == null) ? 0 : satisfyingConditionOperation.hashCode());
-		  result = prime * result + ((satisfyingConditionValue == null) ? 0 : satisfyingConditionValue.hashCode());
-		  result = prime * result + ((type == null) ? 0 : type.hashCode());
-		  result = prime * result + ((unit == null) ? 0 : unit.hashCode());
 		  return result;
 	 }
 
 	 @Override
 	 public boolean equals(Object obj)
 	 {
+		  if (this == obj)
+				return true;
+		  if (!super.equals(obj))
+				return false;
+		  if (getClass() != obj.getClass())
+				return false;
+		  CombinedMetric other = (CombinedMetric) obj;
+		  if (composedBy == null)
+		  {
+				if (other.composedBy != null)
+					 return false;
+		  } else if (!composedBy.equals(other.composedBy))
+				return false;
 		  return true;
 	 }
+
+	 
 
 }
