3ec7ca552bee4ff7e1c5998bbd429b0c9f7a30a1
matteoluzzi <matteo.luzzi@gmail.com>
2015-04-03 10:35:44 +0200

    Added function isIncluded to check whether a value is included into a range of values

--- src/main/java/it/uniroma2/gqm/model/RangeOfValues.java
+++ src/main/java/it/uniroma2/gqm/model/RangeOfValues.java
@@ -180,103 +180,6 @@ public class RangeOfValues extends BaseObject
 		  return result;
 	 }
 
-	 public boolean isIncluded(RangeOfValues other)
-	 {
-		  if (this.defaultRange && other.isDefaultRange()) //both default range. Just check the inclusion natural < integer < real
-				return DefaultRangeOfValuesEnum.valueOf(this.numberType).ordinal() <= DefaultRangeOfValuesEnum.valueOf(other.getNumberType()).ordinal();
- 
-		  else if(this.defaultRange && !other.isDefaultRange()) //always false, a default range can't be included in a custom range (default ranges are only numeric)
-				return false;
-		  
-		  else if(!this.defaultRange && other.isDefaultRange())
-		  {
-				if(!this.isNumeric()) //default range are numeric. a non numeric range can't be included in a numeric one
-					 return false;
-				else //both numeric, just check the number type of the custom one
-					 return DefaultRangeOfValuesEnum.valueOf(this.numberType).ordinal() <= DefaultRangeOfValuesEnum.valueOf(other.getNumberType()).ordinal();
-		  }
-		  else
-		  // both custom range
-		  {
-				if (this.numeric != other.isNumeric())
-					 return false;
-
-				if (!this.numeric || (!this.range && !other.isRange())) // non
-																						  // numeric
-																						  // or both
-																						  // non range
-				{
-					 List<String> values = Arrays.asList(this.rangeValues.split(","));
-					 List<String> otherValues = Arrays.asList(other.getRangeValues().split(","));
-					 for (String value : values)
-					 {
-						  if (!otherValues.contains(value))
-								return false;
-					 }
-					 return true;
-				} else
-				// numeric
-				{
-					 if (!this.range && other.isRange())
-					 {
-						  String[] ranges = other.getRangeValues().split(",");
-						  String[] values = this.rangeValues.split(",");
-						  for (String value : values)
-						  {
-								boolean found = false;
-								for (String range : ranges)
-								{
-									 Range r = new Range(range);
-									 if (r.includes(Double.valueOf(value), DefaultRangeOfValuesEnum.valueOf(other.numberType)))
-									 {
-										  found = true;
-										  break;
-									 }
-								}
-								if (!found)
-									 return false;
-						  }
-						  return true;
-
-					 } else if (this.range && !other.isRange())
-					 {
-						  String[] ranges = this.rangeValues.split(",");
-
-						  List<Double> otherList = RangeOfValues.getOrderedListOfDoubles(other.getRangeValues());
-
-						  for (String range : ranges)
-						  {
-								Range r = new Range(range);
-								if (!r.isIncluded(otherList, DefaultRangeOfValuesEnum.valueOf(this.numberType)))
-									 return false;
-						  }
-						  return true;
-					 } else
-					 {
-						  String[] otherRanges = other.getRangeValues().split(",");
-						  String[] myRanges = this.rangeValues.split(",");
-						  for (String myRange : myRanges)
-						  {
-								boolean found = false;
-								Range myR = new Range(myRange);
-								for (String otherRange : otherRanges)
-								{
-									 Range otherR = new Range(otherRange);
-									 if (otherR.includes(myR, DefaultRangeOfValuesEnum.valueOf(this.numberType), DefaultRangeOfValuesEnum.valueOf(other.getNumberType())))
-									 {
-										  found = true;
-										  break;
-									 }
-								}
-								if (!found)
-									 return false;
-						  }
-						  return true;
-					 }
-				}
-		  }
-	 }
-
 	 @Override
 	 public boolean equals(Object o)
 	 {
@@ -324,7 +227,166 @@ public class RangeOfValues extends BaseObject
 		  return false;
 		  // TESTME
 	 }
+	 
+	 /**
+	  * Check if value is included into the range of values, either when value is a measurement input for the metric or when it's a metric formula output
+	  * @param value The value to check. It can be both a Double or a String
+	  * @param asMeasurementInput
+	  * @return
+	  * @throws NumberFormatException When trying to convert a string into a non valid Double.
+	  */
+	 public boolean isIncluded(Object value, boolean asMeasurementInput) throws NumberFormatException
+	 {		  			
+		  if(this.defaultRange)
+		  {
+			   Double doubleValue;
+				
+				if(asMeasurementInput)
+					 doubleValue = Double.valueOf((String)value);
+				else
+					 doubleValue = (Double) value;
+				
+				if(DefaultRangeOfValuesEnum.valueOf(this.numberType) == DefaultRangeOfValuesEnum.REAL_NUMBERS)
+					 return true;
+				else if(DefaultRangeOfValuesEnum.valueOf(this.numberType) == DefaultRangeOfValuesEnum.INTEGER_NUMBERS)
+					 return doubleValue % 1 == 0;
+				else // DefaultRangeOfValuesEnum.valueOf(this.numberType) == DefaultRangeOfValuesEnum.NATURAL_NUMBER
+					 return doubleValue % 1 == 0 && doubleValue >= 0;
+		  }
+		  else
+		  {
+				if(!this.numeric) //is not numeric and implicitly not range
+				{   
+					 String stringValue = (String) value;
+					 List<String> rangeList = Arrays.asList(this.rangeValues.split(","));
+					 return rangeList.contains(stringValue);
+				}
+				else //is numeric
+				{
+					 if(!this.range) //is not range
+					 { 
+						  String stringValue = (String) value;
+						  List<String> rangeList = Arrays.asList(this.rangeValues.split(","));
+						  return rangeList.contains(stringValue);
+					 }
+					 else //is range
+					 {
+						  Double doubleValue;
+							
+						  if(asMeasurementInput)
+							  doubleValue = Double.valueOf((String)value);
+						  else
+							  doubleValue = (Double) value;
+						  
+						  List<String> rangeList = Arrays.asList(this.rangeValues.split(","));
+						  for(String range : rangeList)
+						  {
+								Range r = new Range(range);
+								if(r.includes(doubleValue, DefaultRangeOfValuesEnum.valueOf(this.numberType)))
+									 return true;
+						  }
+						  return false;
+					 }
+				}		
+		  }
+	 }
+	 
+	 public boolean isIncluded(RangeOfValues other)
+	 {
+		  if (this.defaultRange && other.isDefaultRange()) //both default range. Just check the inclusion natural < integer < real
+				return DefaultRangeOfValuesEnum.valueOf(this.numberType).ordinal() <= DefaultRangeOfValuesEnum.valueOf(other.getNumberType()).ordinal();
+ 
+		  else if(this.defaultRange && !other.isDefaultRange()) //always false, a default range can't be included in a custom range (default ranges are only numeric)
+				return false;
+		  
+		  else if(!this.defaultRange && other.isDefaultRange())
+		  {
+				if(!this.isNumeric()) //default range are numeric. a non numeric range can't be included in a numeric one
+					 return false;
+				else //both numeric, just check the number type of the custom one
+					 return DefaultRangeOfValuesEnum.valueOf(this.numberType).ordinal() <= DefaultRangeOfValuesEnum.valueOf(other.getNumberType()).ordinal();
+		  }
+		  else // both custom range
+		  {
+				if (this.numeric != other.isNumeric())
+					 return false;
 
+				if (!this.numeric || (!this.range && !other.isRange())) // non
+																						  // numeric
+																						  // or both
+																						  // non range
+				{
+					 List<String> values = Arrays.asList(this.rangeValues.split(","));
+					 List<String> otherValues = Arrays.asList(other.getRangeValues().split(","));
+					 for (String value : values)
+					 {
+						  if (!otherValues.contains(value))
+								return false;
+					 }
+					 return true;
+				} else //numeric ranges
+				{
+					 if (!this.range && other.isRange()) //this range of values is not a range and other range is a range
+					 {
+						  String[] ranges = other.getRangeValues().split(",");
+						  String[] values = this.rangeValues.split(",");
+						  for (String value : values) //check if every value from this range of values list is in other range of value ranges
+						  { 
+								boolean found = false;
+								for (String range : ranges)
+								{
+									 Range r = new Range(range);
+									 if (r.includes(Double.valueOf(value), DefaultRangeOfValuesEnum.valueOf(other.numberType)))
+									 {
+										  found = true;
+										  break;
+									 }
+								}
+								if (!found)
+									 return false;
+						  }
+						  return true;
+
+					 } else if (this.range && !other.isRange()) //this range of values is a range and other range is not a range
+					 {
+						  String[] ranges = this.rangeValues.split(",");
+
+						  List<Double> otherList = RangeOfValues.getOrderedListOfDoubles(other.getRangeValues());
+
+						  for (String range : ranges) //check if every values inside this range is in the other range list
+						  {
+								Range r = new Range(range);
+								if (!r.isIncluded(otherList, DefaultRangeOfValuesEnum.valueOf(this.numberType)))
+									 return false;
+						  }
+						  return true;
+					 } else //this range of values is a range and other range is a range
+					 {
+						  String[] otherRanges = other.getRangeValues().split(",");
+						  String[] myRanges = this.rangeValues.split(",");
+						  for (String myRange : myRanges)
+						  {
+								boolean found = false;
+								Range myR = new Range(myRange); 
+								for (String otherRange : otherRanges) //check if every single range from this is included in other range of values
+								{
+									 Range otherR = new Range(otherRange);
+									 if (otherR.includes(myR, DefaultRangeOfValuesEnum.valueOf(this.numberType), DefaultRangeOfValuesEnum.valueOf(other.getNumberType())))
+									 {
+										  found = true;
+										  break;
+									 }
+								}
+								if (!found)
+									 return false;
+						  }
+						  return true;
+					 }
+				}
+		  }
+	 }
+
+	 
 	 /*
 	  * Returns true if the sets are equivalent, i.e. if they contains the same
 	  * elements
