181a549f3bf2138d74447358ea4e306fefb68e7b
Daniele Angelucci <angelucci.dnl@gmail.com>
2015-04-08 13:03:17 +0200

    fixed check in data collection input (only check numeric type mismatch and not rangeofvalues inclusion). Added support for MULTIPLE VALUE metric formula evaluation

--- src/main/java/it/uniroma2/gqm/webapp/controller/MeasurementValidator.java
+++ src/main/java/it/uniroma2/gqm/webapp/controller/MeasurementValidator.java
@@ -1,10 +1,15 @@
 package it.uniroma2.gqm.webapp.controller;
 
 import it.uniroma2.gqm.model.AbstractMetric;
+import it.uniroma2.gqm.model.CollectingTypeEnum;
 import it.uniroma2.gqm.model.Measurement;
 import it.uniroma2.gqm.model.RangeOfValues;
+import it.uniroma2.gqm.model.SimpleMetric;
+import it.uniroma2.gqm.service.MeasurementManager;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import net.objecthunter.exp4j.Expression;
@@ -13,6 +18,7 @@ import net.objecthunter.exp4j.ValidationResult;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -23,6 +29,10 @@ public class MeasurementValidator implements Validator {
 	
     private final transient Log log = LogFactory.getLog(getClass());
 	
+    @Autowired
+    private MeasurementManager measurementManager;
+    
+    
 	public static final String INCOMPATIBLE_SYMBOL_IN_INPUT_VALUE = "The symbol in input value is not mappable with any symbol in (or is out from the set of) the associated Measurement Scale's Range of Values";
 	public static final String INCOMPATIBLE_SYMBOL_IN_OUTPUT_VALUE = "The symbol in output value is not mappable with any symbol in (or is out from the set of) the associated Measurement Scale's Range of Values";
 	public static final String INCOMPATIBLE_VALUE_TYPE = "The symbol in input is not compatible with this range type. Check your input is correct.";
@@ -51,19 +61,37 @@ public class MeasurementValidator implements Validator {
 
 	public boolean checkFormulaEvaluation(Measurement measurement, Errors errors) throws NumberFormatException{
 
-		AbstractMetric metric = measurement.getMetric();
+		SimpleMetric metric = (SimpleMetric) measurement.getMetric();
 		String input_value = measurement.getValue();
 		RangeOfValues rov = metric.getMeasurementScale().getRangeOfValues();
 		
-		//Check input compatibility
-		if(!rov.isIncluded(input_value, true)){ //input not valid
+		//Check input type compatibility for numeric range of values
+		if(rov.isNumeric()){
+			try{
+				Double.valueOf(input_value);
+			}
+			catch(NumberFormatException e){
+				//rov is numeric but the input is not
+				 errors.rejectValue("value", "incompatible input value", INCOMPATIBLE_SYMBOL_IN_INPUT_VALUE);
+				 return false;
+			}
+		}
+		/*DO NOT CHECK INPUT INSIDE rov. CHECK ONLY OUTPUT INSIDE rov
+		 * if(!rov.isIncluded(input_value, true)){ //input not valid
 			 errors.rejectValue("value", "incompatible input value", INCOMPATIBLE_SYMBOL_IN_INPUT_VALUE);
 			 return false;
-		}
+		}*/
 		
+		//retrieve the correct input value (depending on the rangeofvalues-type)
 		Double value;
-		if(!rov.isRange() && !rov.isNumeric())//range of values is not numeric and not range
+		if(!rov.isRange() && !rov.isNumeric()){//range of values is not numeric and not range
 			value = rov.getStringValueAsNumberByIndex(input_value);
+			if(value==-1){
+				//rov is not numeric but the input is (or it has not been found)
+				errors.rejectValue("value", "incompatible input value", INCOMPATIBLE_SYMBOL_IN_INPUT_VALUE);
+				return false;
+			}	
+		}
 		else
 			value = new Double(input_value);
 		
@@ -80,7 +108,22 @@ public class MeasurementValidator implements Validator {
 			Map<String, Double> values = new HashMap<String, Double>();
 			
 			expressionBuilder.variable("_this_");
-			values.put("_this_", value);
+			
+			Double aggregated_values;
+			//multiple value metric: retrieve other measurement and aggregate them with the actual one, before evaluate the formula
+			if(metric.getCollectingType().equals(CollectingTypeEnum.MULTIPLE_VALUE)){
+				List<Measurement> saved_measurements = this.measurementManager.findMeasuremntsByMetric(metric);
+				List<Double> saved_measurements_values = new ArrayList<Double>();
+				for(Measurement m : saved_measurements){
+					saved_measurements_values.add(Double.valueOf(m.getValue()));
+				}
+				//add last measurement (not yet saved in the system)
+				saved_measurements_values.add(value);
+				aggregated_values = AggregatorHandler.executeAggregator(metric.getAggregator(), saved_measurements_values);
+				values.put("_this_", aggregated_values);
+			}
+			else//single value metric
+				values.put("_this_", value);
 			
 			try {
 				Expression expr = expressionBuilder.build().setVariables(
