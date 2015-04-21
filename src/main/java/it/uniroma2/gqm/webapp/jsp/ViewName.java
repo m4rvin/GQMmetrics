b870b6d339388dca9e3ffe148b31842e66d61a0e
Daniele Angelucci <angelucci.dnl@gmail.com>
2015-04-21 12:05:29 +0200

    added info box for combined metric composers list

--- src/main/java/it/uniroma2/gqm/webapp/jsp/ViewName.java
+++ src/main/java/it/uniroma2/gqm/webapp/jsp/ViewName.java
@@ -10,6 +10,7 @@ public class ViewName {
 	public static final String combinedMetricForm = "combinedmetricform";
 	public static final String combinedMetricFormAjax = "combinedmetricformAjax";
 	public static final String combinedMetricFormMeasurementScaleInfoAjax = "combinedmetricformMeasurementScaleInfoAjax";
+	public static final String combinedMetricFormInfoAjax = "combinedmetricformInfoAjax";
 
 	
 	public static final String measurementScales = "measurementScales";
