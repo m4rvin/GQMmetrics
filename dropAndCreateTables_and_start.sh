c3ad3ab66259a658357e87edf0623e0ef80d4428
Daniele Angelucci <angelucci.dnl@gmail.com>
2015-04-24 18:47:23 +0200

    updated db name into gqm_s_ms_prod

--- dropAndCreateTables_and_start.sh
+++ dropAndCreateTables_and_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn jetty:run -P prod -DskipTests=false -Djetty.port=9999 -Ddb.name=GQM_BLIND_PROD
+mvn jetty:run -P prod -DskipTests=false -Djetty.port=9999 -Ddb.name=gqm_s_ms_prod
 
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn jetty:run -P prod -DskipTests=true -Djetty.port=9999 -Ddb.name=GQM_BLIND_PROD
+mvn jetty:run -P prod -DskipTests=true -Djetty.port=9999 -Ddb.name=gqm_s_ms_prod
 
